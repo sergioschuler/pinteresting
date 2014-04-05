fa3893b5f3eddd32912ef634c261a97757603ea0
Sergio Schuler <sergio.schuler@gmail.com>
2014-04-05 11:27:33 -0300

    When user deletes account, pins will be deleted

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :pins
+  has_many :pins, dependent: :destroy
   has_attached_file :avatar, :styles => { :medium => "150x150>", :thumb => "30x30!" }
   validates_presence_of :display_name
 end
